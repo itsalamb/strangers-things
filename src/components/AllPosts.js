3df6b1ae25e749722ef10d39ca04175b092169a2
ItsALamb <austinmillerlambert@gmail.com>
2021-10-31 16:09:57 -0500

    Last effort before I gave up before time contstraints

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 
 const AllPosts = () => {
     const [posts, setPosts] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
   const fetchPosts = async () => {
@@ -21,6 +22,13 @@ const AllPosts = () => {
   fetchPosts();
 }, [])
 
+// function postMatches(post, text) {
+
+// }
+
+// const filteredPosts = posts.filter(post => postMatches(post, searchTerm));
+// const postsToDisplay = searchTerm.length ? filteredPosts : posts;
+
     return (
         <>
            <h1 className="Posts">Posts</h1>
@@ -33,6 +41,21 @@ const AllPosts = () => {
     </div>):null} 
         </>
     );
+
+    // return (
+    //     <>
+    //        <h1 className="Posts">Posts</h1>
+    //         {postsToDisplay.length ? postsToDisplay.map(post => <div key={post._id}>
+    //             <h3>{post.title}</h3>
+    //             <p>{post.description}</p>
+    //             <p>{post.price}</p>
+    //             <p>{post.location}</p>
+    //             <p>{post.willDeliver ? "Willing to Deliver" : "Not Willing to Deliver"}</p>
+    // </div>):null} 
+    //     </>
+    // );
 }
 
 export default AllPosts
+
+//didnt get to the searching function and didnt realize it needed one until late Sunday afternoon
